eeb232eb9df98b53033cb736c624e0838387eec2
Keith Winstein <keithw@cs.stanford.edu>
2018-06-22 16:34:28 -0700

    natclient: more info

--- examples/natclient.cc
+++ examples/natclient.cc
@@ -16,18 +16,16 @@ int main( int argc, char *argv[] )
       abort();
     }
 
-    if ( argc != 2 ) {
-      cerr << "Usage: " << argv[ 0 ] << " IP\n";
-      return EXIT_FAILURE;
-    }
-
     const string remote_ip = argv[ 1 ];
     UDPSocket socket;
     socket.bind( Address( "0", 50000 ) );
 
-    for ( unsigned int i = 0; i < 16; i++ ) {
-      socket.sendto( Address( remote_ip, 60000 + i ),
+    for ( int i = 1; i < argc; i++ ) {
+      const Address dest( argv[ i ], 60000 );
+      cerr << "Sending to " << dest.to_string() << "... ";
+      socket.sendto( Address( argv[ i ], 60000 ),
 		     "Hello, Dima." );
+      cerr << "done.\n";
       this_thread::sleep_for( 50ms );
     }
   } catch ( const exception & e ) {
