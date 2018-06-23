8b8d103c8651c05e1980653a492c1a740582668e
Keith Winstein <keithw@cs.stanford.edu>
2018-06-23 15:30:44 -0700

    echoserver.cc: style tweaks

--- examples/echoserver.cc
+++ examples/echoserver.cc
@@ -1,3 +1,5 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
 #include <vector>
 #include <iostream>
 
@@ -13,25 +15,25 @@ int main(int argc, char** argv)
   try {
     Poller poller;
     UDPSocket socket;
-    int port = 60000;
+    uint16_t port = 60000;
     if ( argc > 1 ) {
-      port = atoi(argv[1]);
+      port = stoi(argv[1]);
     }
 
-    socket.bind( Address( "0", port ) );
+    socket.bind( { "0", port } );
     poller.add_action( Action( socket,
-				 Direction::In,
-				 [&socket] () {
-				   auto rec = socket.recv();
-				   cout << "\tUDP datagram from " << rec.source_address.to_string() << "\n";
-           socket.sendto(rec.source_address, rec.source_address.to_string());
-				   return ResultType::Continue;
-				 } ) );
+                               Direction::In,
+                               [&socket] () {
+                                 auto rec = socket.recv();
+                                 cout << "\tUDP datagram from " << rec.source_address.to_string() << "\n";
+                                 socket.sendto(rec.source_address, rec.source_address.to_string());
+                                 return ResultType::Continue;
+                               } ) );
 
     while ( true ) {
       const auto ret = poller.poll( -1 );
       if ( ret.result == PollResult::Exit ) {
-	      return ret.exit_status;
+        return ret.exit_status;
       }
     }
   } catch ( const exception & e ) {
