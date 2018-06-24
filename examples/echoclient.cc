d0feaba2949643122ea7e471d3af8ca5f9356164
Keith Winstein <keithw@cs.stanford.edu>
2018-06-23 17:54:24 -0700

    Move echoclient source port to 50000

--- examples/echoclient.cc
+++ examples/echoclient.cc
@@ -28,7 +28,7 @@ int main(int argc, char** argv)
 
   try {
     UDPSocket sock;
-    sock.bind( Address( "0", uint16_t( 60000 ) ) );
+    sock.bind( Address( "0", uint16_t( 50000 ) ) );
     
     Poller poller;
     poller.add_action( Action( sock,
